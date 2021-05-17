274f7d2f210354b65e92694629083c8d73ba411b
Chris Schilling <crschilling@gmail.com>
2021-05-17 01:27:20 -0400

    add tailwind typography to render markdown in html using tailwindcss.

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,18 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section>
-        <p>[Your Self Introduction]</p>
+        <p>[Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <br></br>
+        <div class="bg-white dark:bg-gray-800">
+          <h1 class="text-gray-900 dark:text-white">Dark mode is here!</h1>
+          <p class="text-gray-600 dark:text-gray-300">
+            Also in grey...
+          </p>
+        </div>
     </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
